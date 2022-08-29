9b4ca29db1640859443e5e81ba8a8538b97ae9e8
Diogo <quintaodiogo@gmail.com>
2022-08-29 17:06:09 -0300

    Update Search.js

--- Ecommerce-Godev/src/components/Header/Search.js
+++ Ecommerce-Godev/src/components/Header/Search.js
@@ -2,7 +2,9 @@ import React from "react";
 const Search = () => {
   return (
     <div className="search-container">
-      <i className="fas fa-search search"></i>
+      <a className="search-btn" href="#">
+        <i className="fas fa-search search"></i>
+      </a>
       <input type="text" className="input" name="searchBox" placeholder="Pesquisar Produtos" />
     </div>
   );
